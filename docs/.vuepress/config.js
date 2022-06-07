3a59ee5bec37d6dc53a6b3a06dfe0279623c5540
涂威 <tuwei@lonch.com.cn>
2022-06-07 17:35:12 +0800

    配置文件更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 // const { defaultTheme } = require('vuepress')
 const { hopeTheme } = require("vuepress-theme-hope");
-console.log(process.env.NODE_ENV);
 module.exports = {
-  base: process.env.NODE_ENV === 'production' ? '/ui/ms100' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ui/ms100/' : '/',
+  dest: './ms100', //指定输出路径
   // 站点选项
   theme: hopeTheme({
     plugins:{
